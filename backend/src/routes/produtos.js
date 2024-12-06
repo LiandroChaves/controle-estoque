349b83a3753a92a181fdd00a1cff68d6758a6bca
Liandro Chaves <liandrochaves18@gmail.com>
2024-12-06 13:52:59 -0300

    Eu sou fodaaaa caralhooooooooooooooo

--- backend/src/routes/produtos.js
+++ backend/src/routes/produtos.js
@@ -2,39 +2,32 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database/db');
 
-// Rota para listar os produtos
-// Rota para listar os produtos
-// Rota para listar os produtos
 router.get('/produtos', async (req, res) => {
     try {
       const result = await pool.query('SELECT * FROM produtos');
       res.json(result.rows);
     } catch (error) {
-      console.error('Erro ao buscar produtos:', error); // Log detalhado
+      console.error('Erro ao buscar produtos:', error);
       res.status(500).json({
         error: 'Erro ao consultar o banco de dados',
-        message: error.message,  // Mostra a mensagem do erro
-        stack: error.stack,      // Mostra o stack trace completo para depuração
+        message: error.message,
+        stack: error.stack,
       });
     }
   });
   
   
 
-// Rota para criar um novo produto
 router.post('/produtos', async (req, res) => {
   const { nome, categoria, subcategoria, estoque, preco, catalogo, favorito } = req.body;
 
-  // Verificando se os campos obrigatórios foram fornecidos
   if (!nome || !categoria || !subcategoria || !estoque || !preco) {
     return res.status(400).json({ error: 'Faltam dados obrigatórios' });
   }
 
-  // Define o valor padrão para 'favorito' se não for informado
   const favoritoValue = favorito !== undefined ? favorito : false;
 
   try {
-    // Inserir o produto no banco de dados
     const result = await pool.query(
       'INSERT INTO produtos (nome, categoria, subcategoria, estoque, preco, catalogo, favorito) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
       [nome, categoria, subcategoria, estoque, preco, catalogo, favoritoValue]
@@ -46,5 +39,28 @@ router.post('/produtos', async (req, res) => {
     res.status(500).json({ error: 'Erro ao inserir o produto no banco de dados' });
   }
 });
+
+
+router.delete('/produtos', async (req, res) => {
+  try {
+    await pool.query('DELETE FROM produtos');
+    res.status(200).json({ message: 'Todos os produtos foram excluídos' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao excluir produtos' });
+  }
+});
+
+
+router.post('/produtos/reset-sequence', async (req, res) => {
+  try {
+    await pool.query('ALTER SEQUENCE produtos_id_seq RESTART WITH 1');
+    res.status(200).json({ message: 'Sequência reiniciada para 1' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao reiniciar a sequência' });
+  }
+});
+
 
 module.exports = router;
