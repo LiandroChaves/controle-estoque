893187e686e94b8cfbac6228c94214f2f921e249
Liandro Chaves <liandrochaves18@gmail.com>
2024-12-07 11:58:22 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,8 @@
 import Image from "next/image";
-import Produtos from "./components/Produtos";
 import Login from "./components/Login";
 
 export default function Home() {
   return (
     <Login/>
-    // <Produtos/>
   );
 }
